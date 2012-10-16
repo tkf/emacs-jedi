3574f423b45507d83f0cd9616a504118bac19da3
Takafumi Arakaki <aka.tkf@gmail.com>
2012-10-16 22:21:46 +0200

    Add simple auto completion

--- jedi.el
+++ jedi.el
@@ -1,4 +1,5 @@
 (require 'epc)
+(require 'auto-complete)
 
 (defvar jedi:source-dir (if load-file-name
                             (file-name-directory load-file-name)
@@ -22,6 +23,24 @@
 (defun jedi:get-epc ()
   (or jedi:epc (jedi:start-epc)))
 
+
+;;; AC source
+
+(defvar jedi:ac-direct-matches nil
+  "Variable to store completion candidates for `auto-completion'.")
+
+;; (makunbound 'ac-source-jedi-direct)
+(ac-define-source jedi-direct
+  '((candidates . jedi:ac-direct-matches)
+    (symbol . "s")))
+
+(defun jedi:ac-complete (matches)
+  (setq jedi:ac-direct-matches matches)  ; let-binding won't work
+  (auto-complete '(ac-source-jedi-direct)))
+
+
+;;; Completion
+
 (defun jedi:complete (source line column source-path)
   (interactive (list (buffer-substring-no-properties (point-min) (point-max))
                      (count-lines (point-min) (point))
@@ -32,4 +51,4 @@
                        (list source line column source-path))
     (deferred:nextc it
       (lambda (completions)
-        (message "completions = %S" completions)))))
+        (jedi:ac-complete completions)))))

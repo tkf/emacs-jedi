d5a22b98401c25d1b7c1d952c84a5258f17bb4d3
Syohei YOSHIDA <syohex@gmail.com>
2014-01-30 14:20:57 +0900

    Switch to Cask from Carton

--- doc/source/conf.el
+++ doc/source/conf.el
@@ -3,14 +3,14 @@
 
 ;; Load carton.el
 (add-to-list 'load-path
-             (jedi:parent-dir (jedi:parent-dir (executable-find "carton"))))
-(load "carton")
+             (jedi:parent-dir (jedi:parent-dir (executable-find "cask"))))
+(load "cask")
 
-;; Setup `load-path' using carton.el and package.el
+;; Setup `load-path' using cask.el and package.el
 (let* ((doc-source-path (file-name-directory load-file-name))
        (project-path (jedi:parent-dir (jedi:parent-dir doc-source-path))))
   (add-to-list 'load-path project-path)
-  (carton-setup project-path))
+  (cask-setup project-path))
 (package-initialize)
 
 ;; Load Jedi.el
